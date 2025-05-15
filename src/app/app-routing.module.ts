c6c9149f331c2b239af4a99e08128bb4433846b5
sayfthabet <100999278+sayfthabet@users.noreply.github.com>
2025-05-15 19:04:07 +0100

    commit2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddVehiculeSayfeddinethabetComponentComponent } from './add-vehicule-sayfeddinethabet-component/add-vehicule-sayfeddinethabet-component.component';
 import { VehiculeSayfeddinethabetComponentComponent } from './vehicule-sayfeddinethabet-component/vehicule-sayfeddinethabet-component.component';
-// import { NotFoundComponent } from './not-found/not-found.component'; // Uncomment if implemented
 
 const routes: Routes = [
   { path: '', redirectTo: 'vehicules', pathMatch: 'full' },
